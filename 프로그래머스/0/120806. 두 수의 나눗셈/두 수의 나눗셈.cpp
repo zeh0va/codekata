246671cd89c468c170dc1d11c6284ae64fbb831b
zeh0va <zehosea@gmail.com>
2025-08-20 09:29:56 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.01 ms, Memory: 4.23 MB -BaekjoonHub

---
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num1, int num2) {
+    
+    double answer = (double) num1 / num2;
+    
+    answer = answer * 1000;
+    
+    return (int) answer;
+    
+}
